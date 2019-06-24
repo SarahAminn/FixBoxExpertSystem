48b07ef9ff811023e94f1d47f5dfe1506cdf3223
Abdelrahman Bonna <abdobonna@yahoo.com>
2019-06-24 21:11:06 +0200

    edit

--- FIXBOX/FIXBOX/FIXBOX/FIXBOX/WindowsFormsApplication1/Description.cs
+++ FIXBOX/FIXBOX/FIXBOX/FIXBOX/WindowsFormsApplication1/Description.cs
@@ -44,7 +44,7 @@ namespace FIXBOX
                     {
                         DataRow row = dt.Rows[0];
                         string Decs = row["printer_Desc"].ToString();
-                        richTextBox1.Text = Decs.Replace("\\\\n", Environment.NewLine);
+                        richTextBox1.Text = Decs;
                     }
                 }
                 catch (Exception ex) { MessageBox.Show(ex.Message); }
